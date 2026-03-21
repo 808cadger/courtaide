cf7c721b9d34900b1a6c9db7da8189f5f221baf4
cadger808 <cadger808@noreply.codeberg.org>
2026-03-20 19:21:28 -1000

    Debug fixes: courtaide

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE = 'v1-courtaide';
 const PRECACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
